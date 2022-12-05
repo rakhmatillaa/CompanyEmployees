8f57003e46317e57a3e4899583eef6682480163e
rakhmatillaa <abdukadyrovrahmatilla461@gmail.com>
2022-12-05 10:38:42 +0500

    projects Contracts and LoggerService have been added; configured Logger Service for Logging Messages

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,22 +1,51 @@
+using CompanyEmployees.Extensions;
+using NLog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+// Register dependent services here: (ConfigureServices)
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureLoggerService();
 
+// !!!The code below must have been added to the constructor of Startup class
+LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(),
+"/nlog.config"));
+
+var app = builder.Build();
+// Register middleware here: (Configure)  //(don't forget that order matters!)
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
+
+// It enables using static fields for the request. If a path to the static files directory isn't set, a "wwwroot" folder in our project will be used by default
+app.UseStaticFiles();
+
+app.UseCors("CorsPolicy");
+
+// It provides proxy headers to the current request. This helps during application deployment
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.All
+});
+
+app.UseRouting();
 
 app.UseAuthorization();
 
