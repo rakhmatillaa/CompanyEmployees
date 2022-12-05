bb9e12349ff6b4ee34a077a687a27ccacc533a6f
rakhmatillaa <abdukadyrovrahmatilla461@gmail.com>
2022-12-05 11:00:24 +0500

    modified WeatherForecastController

--- CompanyEmployees/Controllers/WeatherForecastController.cs
+++ CompanyEmployees/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using Contracts;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CompanyEmployees.Controllers
@@ -6,28 +7,21 @@ namespace CompanyEmployees.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
+        private ILoggerManager logger;
+        public WeatherForecastController(ILoggerManager logger)
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
-            _logger = logger;
+            this.logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        [HttpGet]
+        public IEnumerable<string> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            logger.LogInfo("Here is info message from our values controller.");
+            logger.LogDebug("Here is debug message from our values controller.");
+            logger.LogWarn("Here is our warning message from our values controller.");
+            logger.LogError("Here is our error message from our values controller.");
+
+            return new string[] { "value1", "value2" };
         }
     }
 }
