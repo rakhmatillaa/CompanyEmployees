547b75979be6bf18b47a384e4ffa8c5dcabadb14
rakhmatillaa <abdukadyrovrahmatilla461@gmail.com>
2022-12-07 17:31:41 +0500

    added AutoMapper

--- CompanyEmployees/Controllers/CompaniesController.cs
+++ CompanyEmployees/Controllers/CompaniesController.cs
@@ -1,4 +1,5 @@
-﻿using Contracts;
+﻿using AutoMapper;
+using Contracts;
 using Entities.DataTransferObjects;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,11 +11,13 @@ namespace CompanyEmployees.Controllers
     {
         private readonly IRepositoryManager _repository;
         private readonly ILoggerManager _logger;
+        private readonly IMapper _mapper;
 
-        public CompaniesController(IRepositoryManager repository, ILoggerManager logger)
+        public CompaniesController(IRepositoryManager repository, ILoggerManager logger,IMapper mapper)
         {
             _repository = repository;
             _logger = logger;
+            _mapper = mapper;
         }
 
         [HttpGet]
@@ -24,12 +27,7 @@ namespace CompanyEmployees.Controllers
             {
                 var companies = _repository.Company.GetAllCompanies(trackChanges: false);
 
-                var companiesDto = companies.Select(c => new CompanyDto
-                {
-                    Id = c.Id,
-                    Name = c.Name,
-                    FullAddress = string.Join(' ', c.Address, c.Country)
-                }).ToList();
+                var companiesDto = _mapper.Map<IEnumerable<CompanyDto>>(companies);
 
                 return Ok(companiesDto);
             }
