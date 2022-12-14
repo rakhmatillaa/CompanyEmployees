70514864b698499066c1b6bc60832505d3bfa602
rakhmatillaa <abdukadyrovrahmatilla461@gmail.com>
2022-12-14 18:52:05 +0500

    created DELETE and PUT operations

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -22,5 +22,10 @@ namespace Repository
             employee.CompanyId = companyId;
             Create(employee);
         }
+
+        public void DeleteEmployee(Employee employee)
+        {
+            Delete(employee);
+        }
     }
 }
