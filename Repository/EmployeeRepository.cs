c7766bde1ead2cf50ee84e24b0b779fdec53d056
rakhmatillaa <abdukadyrovrahmatilla461@gmail.com>
2022-12-19 16:22:01 +0500

    implemented both filtering and searching for employees

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -3,6 +3,7 @@ using Entities;
 using Entities.Models;
 using Entities.RequestFeatures;
 using Microsoft.EntityFrameworkCore;
+using Repository.Extensions;
 
 namespace Repository
 {
@@ -16,6 +17,8 @@ namespace Repository
         public async Task<PagedList<Employee>> GetEmployeesAsync(Guid companyId, EmployeeParameters employeeParameters, bool trackChanges)
         {
             var employees = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
+                .FilterEmployees(employeeParameters.MinAge, employeeParameters.MaxAge)
+                .Search(employeeParameters.SearchTerm)
                 .OrderBy(e => e.Name)
                 .ToListAsync();
 
