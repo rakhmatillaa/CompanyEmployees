987ed0cb80e26f825d2913555d97b6ddc329c68d
rakhmatillaa <abdukadyrovrahmatilla461@gmail.com>
2022-12-22 11:18:36 +0500

    implemented Sorting

--- Repository/Extensions/RepositoryEmployeeExtensions.cs
+++ Repository/Extensions/RepositoryEmployeeExtensions.cs
@@ -1,5 +1,8 @@
 ﻿using Entities.Models;
 using System.Runtime.CompilerServices;
+using System.Linq.Dynamic.Core;
+using System.Reflection;
+using System.Text;
 
 namespace Repository.Extensions
 {
@@ -15,6 +18,39 @@ namespace Repository.Extensions
 
             var lowerCaseTerm = searchTerm.Trim().ToLower();
             return employees.Where(e=>e.Name.ToLower().Contains(lowerCaseTerm));
+        }
+
+        public static IQueryable<Employee> Sort(this IQueryable<Employee> employees, string orderByQueryString)
+        {
+            if(string.IsNullOrWhiteSpace(orderByQueryString)) 
+                return employees.OrderBy(e=>e.Name);
+
+            var orderParams=orderByQueryString.Trim().Split(',');
+            var propertyInfos = typeof(Employee).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var orderQueryBuilder = new StringBuilder();
+
+            foreach(var param in orderParams)
+            {
+                if (string.IsNullOrWhiteSpace(param))
+                    continue;
+
+                var propertyFromQueryName = param.Split(" ")[0];
+                var objectProperty = propertyInfos.FirstOrDefault(pi => pi.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
+
+                if (objectProperty == null)
+                    continue;
+
+                var direction = param.EndsWith(" desc") ? "descending" : "ascending";
+
+                orderQueryBuilder.Append($"{objectProperty.Name.ToString()} {direction},");
+            }
+
+            var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+
+            if (string.IsNullOrWhiteSpace(orderQuery))
+                return employees.OrderBy(e => e.Name);
+
+            return employees.OrderBy(orderQuery);
         }
     }
 }
